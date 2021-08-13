a4ec23480d54b4f183be4141e8c9376e089fe097
Diego Solis <disc3110@gmail.com>
2021-08-12 23:13:15 -0500

    Add events to make calculator work

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,31 +1,51 @@
 /* eslint-disable react/prefer-stateless-function */
 import './Calculator.css';
 import React from 'react';
+import calculate from '../logic/calculate';
 
 class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      calcObj: {
+        total: null,
+        next: 0,
+        operation: null,
+      },
+    };
+  }
+
+  handleEvaluation(button) {
+    const { calcObj } = this.state;
+    const { total, next, operation } = calculate(calcObj, button);
+    this.setState({ calcObj: { total, next, operation } });
+  }
+
   render() {
+    const { calcObj } = this.state;
+    const { next } = calcObj;
     return (
       <div className="container">
-        <div className="screen dg">0</div>
-        <button className="lightgray" type="button">AC</button>
-        <button className="lightgray" type="button">+/-</button>
-        <button className="lightgray" type="button">%</button>
-        <button className="orange" type="button">+</button>
-        <button className="lightgray" type="button">7</button>
-        <button className="lightgray" type="button">8</button>
-        <button className="lightgray" type="button">9</button>
-        <button className="orange" type="button">x</button>
-        <button className="lightgray" type="button">4</button>
-        <button className="lightgray" type="button">5</button>
-        <button className="lightgray" type="button">6</button>
-        <button className="orange" type="button">-</button>
-        <button className="lightgray" type="button">1</button>
-        <button className="lightgray" type="button">2</button>
-        <button className="lightgray" type="button">3</button>
-        <button className="orange" type="button">/</button>
-        <button className="lightgray" type="button" id="zero">0</button>
-        <button className="lightgray" type="button">.</button>
-        <button className="orange" type="button">=</button>
+        <div className="screen dg">{ next }</div>
+        <button className="lightgray" type="button" onClick={() => this.handleEvaluation('AC')}>AC</button>
+        <button className="lightgray" type="button" onClick={() => this.handleEvaluation('+/-')}>+/-</button>
+        <button className="lightgray" type="button" onClick={() => this.handleEvaluation('%')}>%</button>
+        <button className="orange" type="button" onClick={() => this.handleEvaluation('+')}>+</button>
+        <button className="lightgray" type="button" onClick={() => this.handleEvaluation(7)}>7</button>
+        <button className="lightgray" type="button" onClick={() => this.handleEvaluation(8)}>8</button>
+        <button className="lightgray" type="button" onClick={() => this.handleEvaluation(9)}>9</button>
+        <button className="orange" type="button" onClick={() => this.handleEvaluation('x')}>x</button>
+        <button className="lightgray" type="button" onClick={() => this.handleEvaluation(4)}>4</button>
+        <button className="lightgray" type="button" onClick={() => this.handleEvaluation(5)}>5</button>
+        <button className="lightgray" type="button" onClick={() => this.handleEvaluation(6)}>6</button>
+        <button className="orange" type="button" onClick={() => this.handleEvaluation('-')}>-</button>
+        <button className="lightgray" type="button" onClick={() => this.handleEvaluation(1)}>1</button>
+        <button className="lightgray" type="button" onClick={() => this.handleEvaluation(2)}>2</button>
+        <button className="lightgray" type="button" onClick={() => this.handleEvaluation(3)}>3</button>
+        <button className="orange" type="button" onClick={() => this.handleEvaluation('÷')}>÷</button>
+        <button className="lightgray" type="button" id="zero" onClick={() => this.handleEvaluation(0)}>0</button>
+        <button className="lightgray" type="button" onClick={() => this.handleEvaluation('.')}>.</button>
+        <button className="orange" type="button" onClick={() => this.handleEvaluation('=')}>=</button>
       </div>
     );
   }
---
+++ src/logic/calculate.js
@@ -0,0 +1,122 @@
+import operate from './operate';
+
+function isNumber(item) {
+  return typeof item === 'number';
+}
+
+/**
+  * Given a button name and a calculator data object, return an updated
+  * calculator data object.
+  *
+  * Calculator data object contains:
+  *   total:s      the running total
+  *   next:String       the next number to be operated on with the total
+  *   operation:String  +, -, etc.
+  */
+export default function calculate(obj, buttonName) {
+  if (buttonName === 'AC') {
+    return {
+      total: null,
+      next: 0,
+      operation: null,
+    };
+  }
+
+  if (isNumber(buttonName)) {
+    if (buttonName.toString() === '0' && obj.next.toString() === '0') {
+      return { next: 0, total: obj.total, operation: obj.operation };
+    }
+    // If there is an operation, update next
+    if (obj.operation) {
+      if (obj.next) {
+        return (
+          { next: obj.next + buttonName.toString(), operation: obj.operation, total: obj.total }
+        );
+      }
+      return { next: buttonName, operation: obj.operation, total: obj.total };
+    }
+    // If there is no operation, update next and clear the value
+    if (obj.next) {
+      return {
+        next: obj.next + buttonName.toString(),
+        total: null,
+      };
+    }
+    return {
+      next: buttonName,
+      total: null,
+    };
+  }
+
+  if (buttonName === '.') {
+    if (obj.next) {
+      if (obj.next.toString().includes('.')) {
+        return { total: obj.total, next: obj.next, operation: obj.operation };
+      }
+      return { next: `${obj.next}.` };
+    }
+    if (obj.operation) {
+      return { next: '0.' };
+    }
+    if (obj.total) {
+      if (obj.next.toString().includes('.')) {
+        return { total: obj.total, next: obj.next, operation: obj.operation };
+      }
+      return { total: `${obj.total}.` };
+    }
+    return { total: '0.' };
+  }
+
+  if (buttonName === '=') {
+    if (obj.next && obj.operation) {
+      return {
+        total: null,
+        next: operate(obj.total, obj.next, obj.operation),
+        operation: null,
+      };
+    }
+    // '=' with no operation, nothing to do
+    return { next: 0 };
+  }
+
+  if (buttonName === '+/-') {
+    if (obj.next) {
+      return { next: (-1 * parseFloat(obj.next)).toString() };
+    }
+    if (obj.total) {
+      return { total: (-1 * parseFloat(obj.total)).toString() };
+    }
+    return { next: 0 };
+  }
+
+  // Button must be an operation
+
+  // When the user presses an operation button without having entered
+  // a number first, do nothing.
+  if (!obj.next && !obj.total) {
+    return { next: 0 };
+  }
+
+  // User pressed an operation button and there is an existing operation
+  if (obj.operation) {
+    return {
+      total: operate(obj.total, obj.next, obj.operation),
+      next: 0,
+      operation: buttonName,
+    };
+  }
+
+  // no operation yet, but the user typed one
+
+  // The user hasn't typed a number yet, just save the operation
+  if (!obj.next) {
+    return { operation: buttonName };
+  }
+
+  // save the operation and shift 'next' into 'total'
+  return {
+    total: obj.next,
+    next: 0,
+    operation: buttonName,
+  };
+}
